34e52cdf6d890a956416b7c68bec56ce03aa2e29
yurim86 <icho0405@naver.com>
2020-09-13 09:48:21 +0900

    main.js LoadItems 🎈

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 "use strict";
 
+// Fetch the items from the JSON file 🎈
+function loadItems() {
+  return fetch('data/data.json')
+  .then(response => response.json());
+  .then(json => json.items);
+}
+
+
+
+// main
+loadItems()
+  .then((items) => {
+    displayItems(items);
+    setEventListeners(items);
+  })
+  .catch(console.log);
+
 // tshirt를 클릭할 때 👗 👚👖
