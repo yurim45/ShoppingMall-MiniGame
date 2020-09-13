a1f9ec4b8b35e0586dd0a431c16055ae444c8b64
yurim86 <icho0405@naver.com>
2020-09-13 12:34:41 +0900

    main.js SetEventListeners

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 "use strict";
 
 // Fetch the items from the JSON file 🎈
+// JSON파일 업로드
 function loadItems() {
   return fetch("data/data.json")
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
+// Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector(".items");
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
+// Create HTML list item from the given data item
 function createHTMLString(item) {
   return `
     <li class="item">
@@ -21,6 +24,27 @@ function createHTMLString(item) {
   `;
 }
 
+function onBtnClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  console.log(filtered);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".navbar__logo");
+  const btn = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  btn.addEventListener("click", (event) => onBtnClick(event, items));
+}
+
 // main
 loadItems()
   .then((items) => {
@@ -28,5 +52,3 @@ loadItems()
     setEventListeners(items);
   })
   .catch(console.log);
-
-// tshirt를 클릭할 때 👗 👚👖
