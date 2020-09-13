42079a5cdeff79f7283d9ea7cd47a13fa3c90238
yurim86 <icho0405@naver.com>
2020-09-13 10:06:50 +0900

    main.js DisplayItems 🎉

--- main.js
+++ main.js
@@ -2,12 +2,24 @@
 
 // Fetch the items from the JSON file 🎈
 function loadItems() {
-  return fetch('data/data.json')
-  .then(response => response.json());
-  .then(json => json.items);
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
 
+function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+      ${item.gender}, ${item.size} size
+    </li>
+  `;
+}
 
 // main
 loadItems()
